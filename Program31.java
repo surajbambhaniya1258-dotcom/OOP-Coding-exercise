73cf984e0c6983c14d13ee3f0d3beb1c08a45792
surajbambhaniya1258-dotcom <surajbambhaniya1258@gmail.com>
2026-04-29 20:16:01 +0530

    Update Program31.java

--- Program31.java
+++ Program31.java
@@ -2,11 +2,8 @@ import java.util.LinkedList;
 
 public class MusicPlaylist {
     public static void main(String[] args) {
-
-        // Create LinkedList
         LinkedList<String> playlist = new LinkedList<>();
 
-        // 1. Add songs
         playlist.add("Song A");
         playlist.add("Song B");
         playlist.add("Song C");
@@ -16,13 +13,11 @@ public class MusicPlaylist {
         System.out.println("Playlist after adding songs:");
         System.out.println(playlist);
 
-        // 2. Display full playlist
         System.out.println("\nFull Playlist:");
         for (String song : playlist) {
             System.out.println(song);
         }
 
-        // 3. Play first song (remove from front)
         if (!playlist.isEmpty()) {
             String playedSong = playlist.removeFirst();
             System.out.println("\nPlaying: " + playedSong);
@@ -31,7 +26,6 @@ public class MusicPlaylist {
         System.out.println("Playlist after playing first song:");
         System.out.println(playlist);
 
-        // 4. Skip last song (remove from end)
         if (!playlist.isEmpty()) {
             String skippedSong = playlist.removeLast();
             System.out.println("\nSkipped: " + skippedSong);
